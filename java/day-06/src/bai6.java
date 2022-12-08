606d221f1ee10674310f42186bca4528eec0a761
hiennguyen2610 <quanghien486@gmail.com>
2022-12-08 21:07:31 +0700

    fix

--- java/day-06/src/bai6.java
+++ java/day-06/src/bai6.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
 public class bai6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập email: ");
+        String email = sc.nextLine();
+        System.out.print("Nhập số điện thoại: ");
+        String phoneNumber = sc.nextLine();
+        String EMAIL_PATTERN =
+                "^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$";
+
+        char[] arrPhone = phoneNumber.toCharArray();
+        if (arrPhone.length <= 11 && arrPhone.length >= 9) {
+            System.out.println(phoneNumber + ": true");
+        } else {
+            System.out.println(phoneNumber + ": false");
+        }
+        System.out.println(email + ": " + Pattern.matches(EMAIL_PATTERN, email));
+
+    }
 }
