754b39b31192319c3002d583a38152571c1c0c6a
hiennguyen2610 <quanghien486@gmail.com>
2023-02-09 17:28:36 +0700

    lc1859

--- 3.DataStructure&Algorithm/day-03/src/main/java/LeetCode1859.java
+++ 3.DataStructure&Algorithm/day-03/src/main/java/LeetCode1859.java
@@ -1,3 +1,16 @@
 import java.util.Arrays;
 
+public class LeetCode1859 {
+    public static String sortSentence(String s) {
+// "is2 sentence4 This1 a3"
+        String[] words = s.split(" ");
+        String[] newS = new String[words.length];
+        for (String word : words) {
+            int k = word.charAt(word.length()-1 - '0');
+            newS[k-1] = word.substring(0,word.length()-1);
+        }
+        return String.join(" ",newS);
+    }
+}
+
 
