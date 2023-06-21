dee1d629434031e8cd2f52725eb19bfbc0804761
hiennguyen2610 <quanghien486@gmail.com>
2023-06-21 14:13:07 +0700

    update

---
+++ 5.SpringBoot/tiny_task_management/src/main/java/com/example/tiny_task_management/TinyTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiny_task_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTaskManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyTaskManagementApplication.class, args);
+    }
+
+}
---
+++ 5.SpringBoot/tiny_task_management/src/test/java/com/example/tiny_task_management/TinyTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiny_task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
